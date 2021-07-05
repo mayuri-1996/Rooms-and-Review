b5b407580ac7dec02a50606c0bc8f884d3cf1c3d
mayuri-1996 <mayuriroymum80@gmail.com>
2021-07-05 22:20:48 +0530

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('pages.index');
+// });
+
+Route::get('/','PageController@index')->name('pages.index');
